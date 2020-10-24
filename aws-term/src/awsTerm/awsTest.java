8e98d86dad4d7c15a2f9117b318dafe0f5af82c3
baeseongmin <bsm9100@naver.com>
2020-10-24 16:42:26 +0900

    available zones구현중

--- aws-term/src/awsTerm/awsTest.java
+++ aws-term/src/awsTerm/awsTest.java
@@ -1,9 +1,11 @@
 package awsTerm;
 
+import java.util.List;
 import java.util.Scanner;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.cloudhsm.model.ListAvailableZonesResult;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
@@ -15,24 +17,24 @@ public class awsTest {
 	
 	static AmazonEC2 ec2;
 	private static void init() throws Exception {
-	/* 
-	 * The ProfileCredentialsProvider will return your [default]
-	 * credential profile by reading from the credentials file located at
-	 * (~/.aws/credentials).
-	*/
-	ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
-	try {
-		credentialsProvider.getCredentials();
-	} catch (Exception e) {
-		throw new AmazonClientException(
-				"Cannot load the credentials from the credential profiles file. " 
-				+"Please make sure that your credentials file is at the correct " 
-				+"location (~/.aws/credentials), and is in valid format.",e);
-	}
-	ec2 = AmazonEC2ClientBuilder.standard()
-			.withCredentials(credentialsProvider)
-			.withRegion("us-east-1")
-			.build();
+		/* 
+		 * The ProfileCredentialsProvider will return your [default]
+		 * credential profile by reading from the credentials file located at
+		 * (~/.aws/credentials).
+		*/
+		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+		try {
+			credentialsProvider.getCredentials();
+		} catch (Exception e) {
+			throw new AmazonClientException(
+					"Cannot load the credentials from the credential profiles file. " 
+					+"Please make sure that your credentials file is at the correct " 
+					+"location (~/.aws/credentials), and is in valid format.",e);
+		}
+		ec2 = AmazonEC2ClientBuilder.standard()
+				.withCredentials(credentialsProvider)
+				.withRegion("us-east-1")
+				.build();
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -63,10 +65,11 @@ public class awsTest {
 				case 1: 
 					listInstances();
 					break;
+				case 2:
+					availableZones();
 			}
 		}
 	}
-
 	
 	public static void listInstances(){
 		System.out.println("Listing instances....");
@@ -96,4 +99,14 @@ public class awsTest {
 		}
 	}
 	
+	public static void availableZones() {
+		System.out.println("Available zones....");
+		ListAvailableZonesResult request = new ListAvailableZonesResult();
+		List<String> zonesArr = request.getAZList();
+		System.out.println(zonesArr.isEmpty());
+		/*
+		 * for(String str : zonesArr) { System.out.println(str); }
+		 */
+		System.out.println("You have access to" + "Availability Zones");
+	}
 }
